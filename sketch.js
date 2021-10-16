2debda8fc55c0609d14f05450bcc8ff1aff391ed
Dxsbond <82594969+Boomx214@users.noreply.github.com>
2021-10-15 20:50:32 -0400

    adding music

--- sketch.js
+++ sketch.js
@@ -1,16 +1,49 @@
+let lost;
+let cloud;
+let intro;
+function preload(){
+
+lost = loadSound("xxxbond.wav");
+cloud = loadSound("cloud..wav");
+intro = loadSound("intro.wav");
+
+
+}
+
 function setup() {
 noCanvas();
+
+
 }
 
 function draw() {
   background(220);
+document.getElementById("lost").onclick(mouseClicked());
 }
 
 
-function myFunction() {
+function mouseClicked() {
+if(lost.isPlaying()){
+lost.stop();
 
-console.log("deez nuts");
 
+} else if(lost.isPaused()){
+  lost.play();
+}
+if(cloud.isPlaying()){
+  cloud.stop();
+  
+  
+  } else if(cloud.isPaused()){
+    cloud.play();
+  }
+  if(intro.isPlaying()){
+    intro.stop();
+    
+    
+    } else if(intro.isPaused()){
+      intro.play();
+    }
 }
 
 
